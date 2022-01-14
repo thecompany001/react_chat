4730b6c98f64d51fa3e520b0bd466b0c7fe9681d
the company <thecompany001@protonmail.com>
2022-01-13 20:37:33 -0500

    chat, socket

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+
+const Chat = ({ Location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000'
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(Location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        console.log(socket);
+        });
+
     return (
         <h1>Chat</h1>
     )
