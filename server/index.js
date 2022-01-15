5730d28d1dfce4d701e9601f5644872d7632a2cd
the company <thecompany001@protonmail.com>
2022-01-15 14:20:58 -0500

    user to rooms, messaging

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -11,17 +13,16 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection');
-
     socket.on('join', ({ name, room }, callback) => {
         console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room });
 
-        const error = true;
+        if(error) return callback(error);
 
-        if(error) {
-            callback({ error: 'error' });
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
+        socket.broadcast,to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`});
 
-        }
+        socket.join(user.room);
     });
     
     socket.on('disconnect', () => {
