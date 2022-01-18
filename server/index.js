57f3c858905a01d122c4919f79d099ac9ba5658b
the company <thecompany001@protonmail.com>
2022-01-18 16:59:49 -0500

    chat

--- server/index.js
+++ server/index.js
@@ -31,6 +31,8 @@ io.on("connection", (socket) => {
 
     socket.join(user.room);
 
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)})
+
     callback();
   });
 
@@ -38,12 +40,17 @@ io.on("connection", (socket) => {
     const user = getUser(socket.id);
 
     io.to(user.room).emit("message", { user: user.name, text: message });
+    io.to(user.room).emit("roomData", { room: user.room, text: message });
 
     callback();
   });
 
   socket.on("disconnect", () => {
-    console.log("User has left");
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.`})
+    }
   });
 });
 
